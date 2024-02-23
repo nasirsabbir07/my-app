1a785729ac66a91f37b294d7a8fd6fdcc98971a1
Nasir <btbsabbirnas@barytech.com>
2024-02-24 04:10:49 +0600

    Angular component excercise

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { OutputComponent } from './output/output.component';
+import { WarningComponent } from './warning/warning.component';
+import { SuccessComponent } from './success/success.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [			AppComponent,
+      OutputComponent,
+      WarningComponent,
+      SuccessComponent
+   ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
